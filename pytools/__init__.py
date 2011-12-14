6cf12d029d61f2f8013393f5923ea01f51a22677
Andreas Stuhlmueller <andreas@stuhlmueller.info>
2011-12-13 19:59:02 -0800

    Fixed naming conflict.

--- pytools/__init__.py
+++ pytools/__init__.py
@@ -7,6 +7,6 @@ from pytools.file import no_extension, parameterize, pickle_to_file, pickle_from
 from pytools.hash import hash_digest
 from pytools.math import argmin, argmax
 from pytools.serve import serve_string, serve_file
-from pytools.time import timedelta_to_seconds
+from pytools.timing import timedelta_to_seconds
 import pytools.external.asyncproc as asyncproc
 import pytools.external.optfunc as optfunc
