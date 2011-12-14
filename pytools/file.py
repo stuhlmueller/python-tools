868e49b097f0a2f39a9ac596fcf3f24cfbb0d345
Andreas Stuhlmueller <andreas@stuhlmueller.info>
2011-12-13 19:45:35 -0800

    Enable pickling of instancemethods.

--- pytools/file.py
+++ pytools/file.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python
 
-import pickle
 from tempfile import NamedTemporaryFile
+import copy_reg
+import pickle
+import types
+
 
 __all__ = ['pickle_from_file', 'parameterize', 'no_extension', 'pickle_to_file']
 
 
+def _pickle_method(method):
+    func_name = method.im_func.__name__
+    obj = method.im_self
+    cls = method.im_class
+    if func_name.startswith('__') and not func_name.endswith('__'):
+        cls_name = cls.__name__.lstrip('_')
+        if cls_name:
+            func_name = '_' + cls_name + func_name
+    return _unpickle_method, (func_name, obj, cls)
+
+
+def _unpickle_method(func_name, obj, cls):
+    for cls in cls.mro():
+        try:
+            func = cls.__dict__[func_name]
+        except KeyError:
+            pass
+        else:
+            break
+            return func.__get__(obj, cls)
+
+    
 def parameterize(template_filename, parameters):
     """
     Returns a temporary filename that corresponds to a file that is
@@ -36,3 +61,5 @@ def pickle_from_file(filename):
     obj = pickle.load(f)
     f.close()
     return obj
+
+copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
